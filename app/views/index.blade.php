ae2aea366da6100f9502948ed5aebfcb1c82867d
anikolv <anikolov8@gmail.com>
2015-04-17 19:47:18 +0300

    Main store interface - js

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -12,17 +12,41 @@
 
 @section('content')
 
+<div class="row">
+		<div class="col-md-11">
+		</div>
+		<div class="col-md-1">
+			<img src="../assets/images/loader.gif" id="loading-indicator" />
+		</div>
+</div>
+
 @stop
 
 @section('scripts')
 	<script>
+		$('#loading-indicator').hide();
+		
 		$(document).ready(function(){
+
+			$(document).ajaxStart(function() {
+				$('#loading-indicator').show();
+			});
+
+			$(document).ajaxStop(function() {
+				$('#loading-indicator').hide();	
+			});
+			
 	  		$('.slider8').bxSlider({
 	    		mode: 'vertical',
 	    		slideWidth: 300,
 	    		minSlides: 2,
 	    		slideMargin: 10
 	  		});
+
+	  		$.ajax({
+				url : "store/getPhones"
+			}).done(function(data) {
+	  		
 		});
 	</script>		
 @stop
