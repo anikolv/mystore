e07d53c7d75f632bcdeb4591a33a49c3d6047aab
anikolv <anikolov8@gmail.com>
2015-05-06 08:59:47 +0300

    Currency conversion

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -27,6 +27,16 @@ class BaseController extends Controller {
 							'</div>
 							<div class="col-md-4" style="text-align: right;padding-right: 40px;">
 									<div class="btn-group">
+								  		<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false" style="margin-right: 40px;">
+								    	' . trans('user_panel.currency') . ' <span class="caret"></span>
+								  		</button>
+								  		<ul class="dropdown-menu" role="menu">
+								    		<li><a href="">BGN &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="../assets/images/bgflag.png"</a></li>
+								    		<li><a href="">USD &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="../assets/images/ukflag.jpg"</a></a></li>
+								  		</ul>
+								 </div>
+								    			
+								<div class="btn-group">
 								  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false" style="margin-right: 40px;">
 								    ' . trans('user_panel.lang') . ' <span class="caret"></span>
 								  </button>
@@ -39,11 +49,12 @@ class BaseController extends Controller {
 								&nbsp;	
 								&nbsp;
 								<a class="" href="logout" title="Logout"><img src="../assets/images/logout.png" style="height: 40px;margin-top: 6px;"/></a>
-							</div>';
+						</div>';
 		} else {
-			$login_info .= '<div class="col-md-3" style="text-align: left; padding-left: 40px; ">
+			$login_info .= '<div class="col-md-2" style="text-align: left; padding-left: 40px; ">
 								<a class="" href="/" title="Home"><img src="../assets/images/test2.png" style="height: 50px;"/></a>
 							</div>
+					
 							<div class="col-md-5" style="margin-top: 7px;">
 								<form method="post" action="/search" style="display: flex;">
 						            	<input type="text" class="form-control" placeholder="' . trans("user_panel.search") . '" id="query" name="query" value="">
@@ -52,16 +63,18 @@ class BaseController extends Controller {
 						            	</div>
 								</form>
 							</div>
-							<div class="col-md-4" style="text-align: right;padding-right: 40px;">
-						            			<div class="btn-group">
-								  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false" style="margin-right: 40px;">
-								    ' . trans('user_panel.currency') . ' <span class="caret"></span>
-								  </button>
-								  <ul class="dropdown-menu" role="menu">
-								    <li><a href="">BGN &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="../assets/images/bgflag.png"</a></li>
-								    <li><a href="">USD &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="../assets/images/ukflag.jpg"</a></a></li>
-								  </ul>
+						            			
+							<div class="col-md-5" style="text-align: right;padding-right: 40px;">
+						          <div class="btn-group">
+								  		<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false" style="margin-right: 40px;">
+								    	' . trans('user_panel.currency') . ' <span class="caret"></span>
+								  		</button>
+								  		<ul class="dropdown-menu" role="menu">
+								    		<li><a href="">BGN &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="../assets/images/bgflag.png"</a></li>
+								    		<li><a href="">USD &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="../assets/images/ukflag.jpg"</a></a></li>
+								  		</ul>
 								</div>
+								    			
 								<div class="btn-group">
 								  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false" style="margin-right: 40px;">
 								    ' . trans('user_panel.lang') . ' <span class="caret"></span>
@@ -71,10 +84,11 @@ class BaseController extends Controller {
 								    <li><a href="/language/en">English &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="../assets/images/ukflag.jpg"</a></a></li>
 								  </ul>
 								</div>
+								    		
 								<a class="" href="/login" title="' . trans('user_panel.login') . '"><img src="../assets/images/login.png" style="height: 40px;"/></a>
 									&nbsp;&nbsp;&nbsp;
 								<a class="" href="/register" title="' . trans('user_panel.register') . '"><img src="../assets/images/registereduser.png" style="height: 50px;;"/></a>
-							</div>';
+						</div>';
 		}
 		View::share('login_info', $login_info);
 	}
